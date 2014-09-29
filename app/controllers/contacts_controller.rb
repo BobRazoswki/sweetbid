105c14872200542ccacf7ba6beb16724ae9c4e8d
BobRazoswki <bobongithub@gmail.com>
2014-09-29 18:01:21 +0200

    revert

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,17 +7,14 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(params[:contact])
     @contact.request = request
-    byebug
+
     if @contact.deliver
       flash.now[:notice] = 'Thank you for your message. We will contact you soon!'
     else
       flash.now[:error] = 'Cannot send message.'
      render :new
     end
-<<<<<<< HEAD
 
-=======
->>>>>>> parent of f02c599... check
   end
 
 end
