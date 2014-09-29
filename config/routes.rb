7c34a2e673056319e131518181d5bf90e9dd81bf
BobRazoswki <bobongithub@gmail.com>
2014-09-29 17:19:25 +0200

    change route 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
 Rails.application.routes.draw do
-  #get 'contact_forms/new'
 
-  #get 'contact_forms/create'
-
- # get 'contact_form/new'
-
-  #get 'contact_form/create'
-
-  root to: "layouts/application"
+  root to: "pages#home"
 
   match '/contacts',     to: 'contacts#new',             via: 'get'
 resources "contacts", only: [:new, :create]
