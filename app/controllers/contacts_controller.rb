8d76e577a98f05d5f8f6bc45f734aaf787b4e3c3
BobRazoswki <bobongithub@gmail.com>
2014-09-29 18:38:44 +0200

    style form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,9 +1,5 @@
 class ContactsController < ApplicationController
   
-  def new
-    @contact = Contact.new
-  end
-
   def create
     @contact = Contact.new(params[:contact])
     @contact.request = request
