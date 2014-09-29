373988d2abf3cf867bfb7ffe84eae995e0fd095a
BobRazoswki <bobongithub@gmail.com>
2014-09-29 16:04:04 +0200

    add flash notice

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,12 +7,12 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(params[:contact])
     @contact.request = request
-    #if @contact.deliver
-    #  flash.now[:notice] = 'Thank you for your message. We will contact you soon!'
-    #else
-    #  flash.now[:error] = 'Cannot send message.'
-    # render :new
-    #end
+    if @contact.deliver
+      flash.now[:notice] = 'Thank you for your message. We will contact you soon!'
+    else
+      flash.now[:error] = 'Cannot send message.'
+     render :new
+    end
   end
 
 end
